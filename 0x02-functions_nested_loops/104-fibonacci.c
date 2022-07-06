112de0363366a2388ee8410067f75aceea127a0c
nyaliech <ochiengnesbit@yahoo.com>
2022-07-05 22:13:05 -0700

    16. first 100 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -25,8 +25,8 @@ int main(void)
 	g = c % 1000000000;
 	for (a = 92; a < 99; ++a)
 	{
-		printf(", %lu" f + (g / 1000000000));
-		printf("%lu", k2 % 1000000000);
+		printf(", %lu" f, + (g / 1000000000));
+		printf("%lu", g % 1000000000);
 		f = f + e;
 		d = f - d;
 		g = g + e;
