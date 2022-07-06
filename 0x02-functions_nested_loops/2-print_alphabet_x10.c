11c35f3c70598b0014b8c86cd53b5fef4b252be8
nyaliech <ochiengnesbit@yahoo.com>
2022-07-05 17:59:15 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int n;
+	int m;
+
+		for (m = 0; m < 10; m++)
+		{
+			{for n = 'a'; n <= 'z'; n++)
+			_putchar(n);
+		}
+	_putchar('\n');
+}
+}
