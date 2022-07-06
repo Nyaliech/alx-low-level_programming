8566c2c682cab6ac736b6506c3ec85b0c19b1425
nyaliech <ochiengnesbit@yahoo.com>
2022-07-05 18:04:52 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,10 @@ void print_alphabet_x10(void)
 
 		for (m = 0; m < 10; m++)
 		{
-			(for n = 'a'; n <= 'z'; n++)
-			_putchar(n);
+			for (n = 'a'; n <= 'z'; n++)
+			{
+				_putchar(n);
 		}
 	_putchar('\n');
-}
+		}
 }
