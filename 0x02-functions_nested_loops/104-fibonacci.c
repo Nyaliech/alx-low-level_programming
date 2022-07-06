916d288623b71d17e62dbb1c43f0d7853e26323a
nyaliech <ochiengnesbit@yahoo.com>
2022-07-05 22:09:29 -0700

    16. first 100 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - finds and prints the first 98 Fibonacci numberst
+ * starting with 1 and 2
+ * followed by a new line
+ * Return: Always0 (Success)
+ */
+int main(void)
+{
+	unsigned long int a, b, c, d, e, f, g;
+
+	b = 1;
+	c = 2;
+
+	printf("%lu", b);
+	for (a = 1; a < 91; a++)
+	{
+		printf(", %lu", c);
+		c = c + b;
+		b = c - b;
+	}
+	d = b / 1000000000;
+	e = b % 1000000000;
+	f = c / 1000000000;
+	g = c % 1000000000;
+	for (a = 92; a < 99; ++a)
+	{
+		printf(", %lu" f + (g / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		f = f + e;
+		d = f - d;
+		g = g + e;
+		e = g - e;
+	}
+	printf("\n");
+	return (0);
+}
