eae995b39213683a00f1632a893dfe1069fdb870
nyaliech <ochiengnesbit@yahoo.com>
2022-07-05 18:21:06 -0700

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabeti letters
+ * @c is a letter to be checked if lower or uppercase
+ * Return: 1 if c is a letter either lower or upper otherwise 0
+ */
+int _salpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || ( >= 'A' && c <= 'Z'));
+}
