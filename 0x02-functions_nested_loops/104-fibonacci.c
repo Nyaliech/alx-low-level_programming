b40528ac78837ce60de0a18beb22f895bf795e61
nyaliech <ochiengnesbit@yahoo.com>
2022-07-06 17:05:28 -0700

    16. AWrite a program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,50 +1,27 @@
 #include <stdio.h>
-
 /**
- * main - prints the first 98 Fibonnacci numbers
- *
- * Return: Always 0 (success)
+ * main - prints the 98 numbers of fibonacci
+ * followed by a new line
+ * Return: ALways 0 (Success)
  */
 int main(void)
 {
-	int x, hold, hold2;
-	long int a, b, c, d, e, f;
+	unsigned long int a = 0, b = 1, c;
+	int x;
 
-	a = 1;
-	b = 2;
-	hold = hold2 = 1;
-	printf("%ld, %ld", a, b);
-	for (x = 1; x <= 96; x++)
+	for (x = 1; x <= 98; x++)
+{
+	c = a + b;
+	if (x != 98)
 	{
-		if (hold)
-		{
-			c = a + b;
-		printf(", %ld", c);
-		a = b;
-		b = c;
+		printf("%lu, ", c);
 	}
 	else
 	{
-		if (hold2)
-		{
-			e = a % 1000000000;
-			f = b % 1000000000;
-			a = a / 1000000000;
-			b = b / 1000000000;
-			hold2 = 0;
-		}
-		d = (e + f);
-		d = a + b + (d / 1000000000);
-		printf(", %ld", c);
-		printf("%ld", d % 1000000000);
-		a = b;
-		e = f;
-		b = c;
-		f = (d % 1000000000);
-				}
-				if (((a + b) < 0) && hold == 1)
-				hold = 0;
-				}
-				printf("\n");
-				return (0);
-				}
+		printf("%lu\n", c);
+	}
+	a = b;
+	b = c;
+}
+return (0);
+}
---
+++ 0x02-functions_nested_loops/104-fibonacci2.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 98 Fibonnacci numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x, hold, hold2;
+	long int a, b, c, d, e, f;
+
+	a = 1;
+	b = 2;
+	hold = hold2 = 1;
+	printf("%ld, %ld", a, b);
+	for (x = 1; x <= 988888888; x++)
+	{
+		if (hold)
+		{
+			c = a + b;
+		printf(", %ld", c);
+		a = b;
+		b = c;
+	}
+	else
+	{
+		if (hold2)
+		{
+			e = a % 1000000000;
+			f = b % 1000000000;
+			a = a / 1000000000;
+			b = b / 1000000000;
+			hold2 = 0;
+		}
+		d = (e + f);
+		d = a + b + (d / 1000000000);
+		printf(", %ld", c);
+		printf("%ld", d % 1000000000);
+		a = b;
+		e = f;
+		b = c;
+		f = (d % 1000000000);
+				}
+				if (((a + b) < 0) && hold == 1)
+				hold = 0;
+				}
+				printf("\n");
+				return (0);
+				}
