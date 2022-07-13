3202c9fe5f7ef171cd25971521f570e7ef96344f
nyaliech <ochiengnesbit@yahoo.com>
2022-07-13 15:41:18 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,7 @@
 char *cap_string(char *a)
 {
 	int kwan = 0, b;
-	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int gawanya[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	if (*(a + kwan) >= 97 && *(a + kwan) <= 122)
 		*(a + kwan) = *(a + kwan) - 32;
@@ -19,10 +19,10 @@ char *cap_string(char *a)
 	{
 		for (b = 0; b < 13; b++)
 		{
-			if (*(b + kwan) == sep_words[b])
+			if (*(a + kwan) == gawanya[b])
 			{
-				if ((*(a + (kwan + 1)) >= 97) && (*(b + (kwan + 1)) <= 122))
-					*(b + (kwan + 1)) = *(b + (kwan + 1)) - 32;
+				if ((*(a + (kwan + 1)) >= 97) && (*(a + (kwan + 1)) <= 122))
+					*(a + (kwan + 1)) = *(a + (kwan + 1)) - 32;
 				break;
 			}
 		}
