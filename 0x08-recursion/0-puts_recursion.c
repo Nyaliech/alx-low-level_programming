b0b9c148393f1a791099213e0c6b7406a25df058
nyaliech <ochiengnesbit@yahoo.com>
2022-07-20 15:11:06 -0700

    prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,13 +8,11 @@
  */
 void _puts_recursion(char *s)
 {
-	s = "lpa\0"
-		if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
-		return;
-	}
 		_putchar(*s);
-		s++;
-			_puts_recursion(s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
 }
